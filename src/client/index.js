f353fe6bbfdcf0b8133513a5263c7a217049585f
Yannik Jozefowski <yannik.jozefowski@alexanderthamm.com>
2021-08-08 14:00:29 +0200

    scss splitted up

--- src/client/index.js
+++ src/client/index.js
@@ -12,7 +12,10 @@ import {
 import './styles/style.scss'
 import './styles/header.scss'
 import './styles/footer.scss'
-import './styles/content.scss'
+import './styles/base-content.scss'
+import './styles/tablet-content.scss'
+import './styles/desktop-content.scss'
+import './styles/desktop2-content.scss'
 
 export {
     getWeather,
